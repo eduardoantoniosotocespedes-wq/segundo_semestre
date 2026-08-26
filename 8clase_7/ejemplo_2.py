bbda4c844e1ed7212b42901502aae22395eedf1f
EduardoSoto <eduardoantoniosotocespedes@gmail.com>
2026-08-26 09:16:08 -0400

    eduardo

---
+++ 8clase_7/ejemplo_2.py
@@ -0,0 +1,6 @@
+n1 = int(input("Ingrese el primer numero: "))
+n2 = int(input("Ingrese el segundo numero: "))
+def suma(n1,n2):
+    res = n1 + n2
+    print(res)    
+suma(n1,n2)    
---
+++ 8clase_7/ejemplo_22.py
@@ -0,0 +1,4 @@
+def suma(n1,n2):
+    res = n1 + n2
+    print(res)    
+suma(10,3)    
