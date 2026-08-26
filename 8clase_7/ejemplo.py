df8b132a9cb1e6892d9e879f024b4d06166566e4
EduardoSoto <eduardoantoniosotocespedes@gmail.com>
2026-08-26 08:45:25 -0400

    commit de prueba

---
+++ 8clase_7/ejemplo.py
@@ -0,0 +1,8 @@
+# apprendiendoeluso correctode variables locales y variables globales
+# creando funciones
+h = "felix"
+def saludar():
+    nombre = "moises"
+    print(nombre)
+saludar()    
+print(h)
---
+++ 8clase_7/ejemplo_1.py
@@ -0,0 +1,4 @@
+n= str(input("///Ingrese su nombre porfavor: "))
+def saludar(n):
+    print(f"///Bienvenido {n}")
+saludar(n)   
